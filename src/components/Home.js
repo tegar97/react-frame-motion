703404813795ce247e5dcedbe5f475f15d8ecd5a
Muhammad Tegar Akmal <eategar@gmail.com>
2021-03-27 21:26:44 +0700

    yoyo hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,19 @@
 import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import {motion} from 'framer-motion'
+
+const buttonVarians = {
+
+    hover : {
+        scale: 1.1,
+        textShadow: '0px 0px 8px rgba(255,255,255)',
+        boxShadow: '0px 0px 8px rgba(255,255,255)' ,
+        transition: {
+            yoyo: Infinity
+        }
+    }
+    
+}
 const Home = () => {
   return (
     <motion.div className="home container" initial={{rotateZ: 180,opacity: 0}}     transition={{ duration: 1 }}
@@ -12,7 +25,7 @@ const Home = () => {
       Welcome to Pizza Joint
     </motion.h2>
       <Link to="/base">
-        <motion.button whileHover={{scale: 1.1,textShadow: '0px 0px 8px rgba(255,255,255)',boxShadow: '0px 0px 8px rgba(255,255,255)' }}>
+        <motion.button variants={buttonVarians} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
